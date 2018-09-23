6d76763fed1c6758b39ed2816ff25ad8091f3e0c
Markov Alexey <mark@delightsoft.ru>
2018-09-23 09:32:09 +0300

    rebuilding site воскресенье, 23 сентября 2018 г. 09:32:09 (MSK)

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,52 @@
 #!/bin/bash
 
+# функция выхода из скрипта при ошибке
+is_err () {
+    [ $? -ne 0 ]
+}
+
+git_push(){
+    git add .
+    if is_err; then return; fi
+    msg="rebuilding site `date`"
+    if [ $# -eq 1 ]
+      then msg="$1"
+    fi
+    git commit -m "$msg"
+    if is_err; then return; fi
+    git push origin master
+    if is_err; then return; fi
+}
+
+git_pull(){
+    git stash
+    if is_err; then return; fi
+    git checkout master
+    if is_err; then return; fi
+    git pull
+    if is_err; then return; fi
+    git stash apply
+    if is_err; then return; fi
+    git_push
+}
+
+
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+# обновление из git основного проекта
+echo -e "\033[0;32m dev git pull...\033[0m"
+git_pull
+if is_err; then return; fi
+git_push
+if is_err; then return; fi
+
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo
 
-# Go To Public folder
 cd public
-# Add changes to git.
-git add .
+# отправляем сгенерированный проект в удаленный репозиторий
+echo -e "\033[0;32m dev git push...\033[0m"
+git_push
+if is_err; then return; fi
 
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin master
-
-# Come Back up to the Project Root
 cd ..
---
+++ init_project.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir nla_site
+cd nla_site
+git clone https://github.com/pepelazz/nl-a.git .
+
+mkdir public
+cd public
+git clone https://github.com/pepelazz/nl-a_site.git .
+
+cd ../..
